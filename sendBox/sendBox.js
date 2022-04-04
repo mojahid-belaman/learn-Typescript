93fd7bae1728514b6e684cf8c21d38a3e1ac1f79
mojahid-belaman <mojahidbelaman@gmail.com>
2022-04-04 18:20:45 +0000

    first commit

---
+++ sendBox/sendBox.js
@@ -0,0 +1,7 @@
+var myStr = "Hello World";
+var myNum = 42;
+var myBool = true;
+myBool = 'test';
+console.log(myStr);
+console.log(myNum);
+console.log(myBool);
---
+++ sendBox/sendBox.ts
@@ -0,0 +1,11 @@
+let myStr = "Hello World";
+
+let myNum = 42;
+
+let myBool = true;
+
+myBool = 'test';
+
+console.log(myStr);
+console.log(myNum);
+console.log(myBool);
